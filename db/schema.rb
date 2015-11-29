e0e8bfb3625d1736ba135c8efc8c11acb849eb1c
brnagn7 <brnagn7@gmail.com>
2015-11-29 20:32:33 +1100

    Added contacts table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125232344) do
+ActiveRecord::Schema.define(version: 20151129092258) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "photos", force: :cascade do |t|
     t.string   "title"
