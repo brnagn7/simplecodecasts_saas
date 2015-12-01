448b94a5873459280b01c1eedf554a5ec1b50e35
brnagn7 <brnagn7@gmail.com>
2015-12-01 19:18:21 +1100

    Added Contact Mailer capability

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,9 @@
+class ContactMailer < ActionMailer::Base
+  default to: 'brnagn7@gmail.com'
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    mail(from: email, subject: 'Spyderops Contact Form Message')
+  end
+end
